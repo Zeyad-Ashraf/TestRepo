5aff26345497b1813fe257bac98a84fbbf7e35ff
Ziad <ziaddash33@gmail.com>
2025-09-23 19:38:23 +0300

    best practices of commits

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// first commit for testing
-// second commit for testing
-// third for another branch
-// fourth for another branch
-// fifth for another branch
-// sixth for another branch
-// seventh for another branch
+
+// feat: add new feature
+// EX => feat(auth): create-login&register-api's
+
+// fix: bug fix 
+// EX => fix(schedule): solution for selecting the same slots
+
+// refactor: refactoring code
+// EX => refactor(auth): refactoring the code into clean architecture
+
+// pref: preformance improvement
+// EX => pref(auth): reselecting only wanted fields from db instead of all
+
+// chore: updating npm packages Ex -> update node version form 20.x to 22.x
+// EX => chore(deps): update all the dependencies
+
+// revert: reverting the commit
+// EX => revert: feat(auth): create-login&register-api's
