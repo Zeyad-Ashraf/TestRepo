d73e9cb5a8058a6c61de5097a75a68998e097cc3
ziad <ziaddash33@gmail.com>
2025-09-23 23:16:52 +0300

    add Notes File

---
+++ Notes.js
@@ -0,0 +1,12 @@
+// how can i use Google Oauth and the receptionist who createing patients accounts with it's mail too ?
+// ask about tenant isolation ? technical term with domain or id or what?
+// who decide the activiation of the doctor ?
+// what do u mean by the doc verify his account ?
+// Inactivity doesn't trigger auto-logout ?????????
+// Patients cann't access patient portals beyond basic appointment reminders !!!
+// what are the steps to verify a patient's email address after account creation?
+// why receptionist login with Google Oauth if the admin will create his account manually?
+// why after 30 M without any requestes the receptionist session expires? much noise for nothing
+// why after 90 days must change Password ? perfromance headache
+// doctor work at different clinics and all of them on my system ?
+// reset links ???
